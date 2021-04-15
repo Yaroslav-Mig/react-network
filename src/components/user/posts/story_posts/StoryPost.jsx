4ed4afc5b8a24ae618a6146faad7e760ea595477
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2021-04-15 20:41:24 +0300

    feat: add props for userStoryPost component

--- src/components/user/posts/story_posts/StoryPost.jsx
+++ src/components/user/posts/story_posts/StoryPost.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './StoryPosts.module.css';
 import userAvatar from '../../../../assets/user_avatar/user-male.png';
 
-export function UserStoryPost() {
+export function UserStoryPost(props) {
+  const { postText, postDate, userName } = props;
   return (
     <article className={styles.module}>
       <header className={styles.header}>
@@ -11,15 +12,15 @@ export function UserStoryPost() {
         </a>
         <div className={styles.info}>
           <h3 className={styles.author}>
-            <a href='#'>User name</a>
+            <a href='#'>{userName}</a>
           </h3>
           <p className={styles.date}>
-            posted on <time datetime='1914-12-20 08:00'>May 17</time>
+            posted on <time datetime='1914-12-20 08:00'>{postDate}</time>
           </p>
         </div>
       </header>
       <div className={styles.content}>
-        <p className={styles.text}>I agree, Germany is my favorite country.</p>
+        <p className={styles.text}>{postText}</p>
       </div>
     </article>
   );
--- src/components/user/posts/story_posts/StoryPosts.jsx
+++ src/components/user/posts/story_posts/StoryPosts.jsx
@@ -4,10 +4,9 @@ import { UserStoryPost } from './StoryPost';
 
 export function UserStoryPosts() {
   return (
-		<div className={styles.wall}>
-			<UserStoryPost/>
-			<UserStoryPost/>
-			<UserStoryPost/>
+    <div className={styles.wall}>
+      <UserStoryPost userName='Alex' postText='I agree, Germany is my favorite country' postDate='May 17' />
+      <UserStoryPost userName='Roy' postText='It was my first trip' postDate='May 23' />
     </div>
   );
 }
