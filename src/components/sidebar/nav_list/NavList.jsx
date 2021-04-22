5dfa146168bf6a33b40071800907129409660164
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2021-04-22 22:43:16 +0300

    feat: add active class for navbar link

--- src/components/sidebar/nav_list/NavList.jsx
+++ src/components/sidebar/nav_list/NavList.jsx
@@ -8,42 +8,42 @@ export function SidebarNavList() {
       <h2 className={styles.heading}>My profile</h2>
       <ul className={styles.box_link}>
         <li>
-          <NavLink className={styles.item_link} to='/profile'>
+          <NavLink className={styles.item_link} activeClassName={styles.active_link} to='/profile'>
             Profile
           </NavLink>
         </li>
         <li>
-          <NavLink className={styles.item_link} to='/about'>
+          <NavLink className={styles.item_link} activeClassName={styles.active_link} to='/about'>
             About
           </NavLink>
         </li>
         <li>
-          <NavLink className={styles.item_link} to='/feed'>
+          <NavLink className={styles.item_link} activeClassName={styles.active_link} to='/feed'>
             News
           </NavLink>
         </li>
         <li>
-          <NavLink className={styles.item_link} to='/messenger'>
+          <NavLink className={styles.item_link} activeClassName={styles.active_link} to='/messenger'>
             Messenger
           </NavLink>
         </li>
         <li>
-          <NavLink className={styles.item_link} to='/friends'>
+          <NavLink className={styles.item_link} activeClassName={styles.active_link} to='/friends'>
             Friends
           </NavLink>
         </li>
         <li>
-          <NavLink className={styles.item_link} to='/groups'>
+          <NavLink className={styles.item_link} activeClassName={styles.active_link} to='/groups'>
             Communities
           </NavLink>
         </li>
         <li>
-          <NavLink className={styles.item_link} to='/photos'>
+          <NavLink className={styles.item_link} activeClassName={styles.active_link} to='/photos'>
             Photos
           </NavLink>
         </li>
         <li>
-          <NavLink className={styles.item_link} to='/settings'>
+          <NavLink className={styles.item_link} activeClassName={styles.active_link} to='/settings'>
             Settings
           </NavLink>
         </li>
