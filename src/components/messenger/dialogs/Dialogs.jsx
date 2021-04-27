e26e8dc47037b13cf80e1c24039ac5d080c80f90
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2021-04-27 23:55:52 +0300

    feat: use map func to dynamic display date in Dialogs component

--- src/components/messenger/dialogs/Dialogs.jsx
+++ src/components/messenger/dialogs/Dialogs.jsx
@@ -6,22 +6,11 @@ export function MessengerDialogs() {
   const dialogsPreview = [
     { id: 1, userName: 'Garry', date: 'April 6', text: 'Ridiculous' },
     { id: 2, userName: 'Bilbo', date: 'May 17', text: 'Butt rider' },
-  ];
+	];
+	
+  const dialogsElements = dialogsPreview.map((dialog) => (
+    <MessengerDialog userName={dialog.userName} idPath={dialog.id} date={dialog.date} textPreview={dialog.text} />
+  ));
 
-  return (
-    <ul className={globalStyles.box}>
-      <MessengerDialog
-        userName={dialogsPreview[0].userName}
-        idPath={dialogsPreview[0].id}
-        date={dialogsPreview[0].date}
-        textPreview={dialogsPreview[0].text}
-      />
-      <MessengerDialog
-        userName={dialogsPreview[1].userName}
-        idPath={dialogsPreview[1].id}
-        date={dialogsPreview[1].date}
-        textPreview={dialogsPreview[1].text}
-      />
-    </ul>
-  );
+  return <ul className={globalStyles.box}>{dialogsElements}</ul>;
 }
